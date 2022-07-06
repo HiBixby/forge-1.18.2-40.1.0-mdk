a82eb32d35dd0c5cb58b0707b2f673f6799d6543
HiBixby <fpssj@naver.com>
2022-07-07 02:08:20 +0900

    rename: 모드 이름을 TutorialMod로 변경

--- src/main/java/io/github/hibixby/tutorialmod/TutorialMod.java
+++ src/main/java/io/github/hibixby/tutorialmod/TutorialMod.java
@@ -1,33 +1,24 @@
-package io.github.hibixby.custom_music_disc;
+package io.github.hibixby.tutorialmod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(CustomMusicDisc.MOD_ID)
-public class CustomMusicDisc
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod
 {
-    public static final String MOD_ID = "custommusicdisc";
+    public static final String MOD_ID = "tutorialmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
 
-    public CustomMusicDisc()
+    public TutorialMod()
     {
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
---
+++ src/main/java/io/github/hibixby/tutorialmod/item/ModItems.java
@@ -0,0 +1,12 @@
+package io.github.hibixby.tutorialmod.item;
+
+import io.github.hibixby.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+}
