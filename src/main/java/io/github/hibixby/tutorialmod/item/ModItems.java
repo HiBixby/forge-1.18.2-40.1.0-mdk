f0569d54f7679ccb691d72000c48af9c8f7ce29f
HiBixby <fpssj@naver.com>
2022-07-07 02:44:02 +0900

    feat: 황수정을 MISC 탭에 추가

--- src/main/java/io/github/hibixby/tutorialmod/item/ModItems.java
+++ src/main/java/io/github/hibixby/tutorialmod/item/ModItems.java
@@ -1,12 +1,25 @@
 package io.github.hibixby.tutorialmod.item;
 
 import io.github.hibixby.tutorialmod.TutorialMod;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
+
+    public static final RegistryObject<Item> CITRINE=ITEMS.register("citrine",
+            ()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC))); //괄호 안 아이템 이름은 소문자로 적어야한다.
+
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 }
